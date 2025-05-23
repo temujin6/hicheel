93f06914c7a987b2d4b71b811ed13e7ecc81949e
temujin <temvjintemvjin6@gmail.com>
2025-05-23 10:12:32 +0800

    update

---
+++ src/pages/video1.js
@@ -0,0 +1,44 @@
+export default function Video1() {
+  return (
+    <div className="flex items-center justify-center flex-col h-screen">
+      <p className="font-bold text-indigo-400 text-2xl mb-2">Бичлэг 1</p>
+      {/* <video className="w-full max-w-2xl rounded-lg shadow-lg" controls>
+          <source src="/video1.mp4" type="video/mp4" />
+        </video> */}
+      {/* <div className="relative w-full max-w-2xl mx-auto pb-[56.25%]"> */}
+      <iframe
+        // className="absolute inset-0 w-full h-full rounded-lg shadow-lg"
+        src="https://www.youtube.com/embed/G1YEAE8OuVA"
+        title="YouTube video player"
+        // frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        // allowFullScreen
+        height={500}
+        width={1000}
+      />
+      {/* </div> */}
+
+      <div className="flex items-center mt-4 space-x-4">
+        <div className="bg-gray-200 rounded-xl p-2">
+          <p className="font-semibold text-lg">Даалгавар</p>
+          <p>1.email, age гараас утга авах</p>
+          <p>2.үүсгэсэн харилцагчийн датаг харуулах</p>
+        </div>
+        <div className="bg-gray-200 rounded-xl p-2">
+          <p className="font-semibold text-lg">Жич</p>
+          <p>
+            github:{" "}
+            <a
+              href="https://github.com/temujin6/11-back"
+              className="text-blue-500"
+            >
+              https://github.com/temujin6/11-back
+            </a>
+          </p>
+          <p>1. createUser --- харилцагч шинээр үүсгэх</p>
+          <p>2. user --- харилцагчийн дата авах</p>
+        </div>
+      </div>
+    </div>
+  );
+}
